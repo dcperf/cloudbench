fffe7bb867efe15d6a7bec327854a3fed042c8d8
Yin Congmin <congmin.yin@intel.com>
2020-08-14 16:46:27 +0800

    test/ceph-fio: add ceph test script and fio configuration
    test ceph cluster performance including iops, band width, latency
    
    Signed-off-by: Yin Congmin <congmin.yin@intel.com>

---
+++ test/ceph-fio/get-lat.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright(c) 2020 Yin, Congmin <congmin.yin@intel.com>
+
+date
+fio_size=(4 16 64)
+fio_depth=(1 8 16 32)
+fio_jobs=(1)
+
+#echo "" > lat.csv
+i=0
+j=0
+k=0
+while (($i<3))
+do
+	#echo size =  ${fio_size[$i]}
+	j=0
+	k=0
+	while (($j<4))
+	do
+		#echo depth =  ${fio_depth[$j]}
+		k=0
+		while (($k<1))
+		do
+			#echo size =  ${fio_size[$i]} depth =  ${fio_depth[$j]} jobs = ${fio_jobs[$k]}
+			export FIO_SIZE=${fio_size[$i]}
+			export FIO_DEPTH=${fio_depth[$j]}
+			export FIO_JOBS=${fio_jobs[$k]}
+			#echo $FIO_SIZE $FIO_DEPTH $FIO_JOBS
+			echo $(cat env-$FIO_SIZE-$FIO_DEPTH-$FIO_JOBS.txt | grep " lat (usec):" | gawk -F, '{print $3}' | gawk -F'=' '{print $2}' | head -n 1)
+			let "k++"
+		done
+		let "j++"
+	done
+	let "i++"
+done
+
+date
